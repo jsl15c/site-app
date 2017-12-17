27812436ecc4faf386f50ffa26cd537d11906c69
Jarrod Luca <jarrodluca@gmail.com>
2017-12-17 04:53:24 -0500

    initial commit

---
+++ models/user-model.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema (
+{
+  firstName: {
+    type: String,
+    required:true
+  },
+  lastName: {
+    type: String,
+    required:true
+  },
+  email: {
+    type: String,
+    required:true
+  },
+  password: {
+    type: String,
+    required:true
+  },
+  userType:{
+    type:String,
+    default:'patient'
+  },
+  emailCode:{
+    type:String
+  },
+  verified: {
+    type:Boolean,
+    default:false
+  }
+},
+{
+  timestamps:true
+}
+);
+
+const UserModel = mongoose.model('User', userSchema);
+
+module.exports = UserModel;
